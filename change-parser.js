1f4fab6ff658a2c1c8c6ff9b35732bf3679067df
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-02-28 13:25:03 +0000

    learning recast

---
+++ change-parser.js
@@ -0,0 +1,44 @@
+const recast = require("recast");
+
+const code = `
+  function add(a, b) {
+    return a * b;
+  }
+`
+;
+
+console.log(`input code:\n${code}`);
+// Let us transform the order of the parameters and convert it in a functionExpression
+
+// Parse the code using an interface similar to require("acorn").parse.
+const ast = recast.parse(code, {
+  parser: require("acorn")
+});
+
+const add = ast.program.body[0];
+
+debugger;
+
+const n = recast.types.namedTypes;
+n.FunctionDeclaration.assert(add);
+
+// If you choose to use recast.builders to construct new AST nodes, all builder
+// arguments will be dynamically type-checked against the Mozilla Parser API.
+const B = recast.types.builders;
+
+// This kind of manipulation should seem familiar if you've used Esprima or the
+// Mozilla Parser API before.
+ast.program.body[0] = B.variableDeclaration("const", [
+  B.variableDeclarator(add.id, B.functionExpression(
+    null, // Anonymize the function expression.
+    add.params,
+    add.body
+  ))
+]);
+
+// Switch the parameters order:
+add.params.push(add.params.shift());
+
+const output = recast.print(ast).code;
+
+console.log(`output code:\n${output}`);
